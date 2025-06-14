3d232e90216c1ce391ff107865a10a137cb68c92
dwipandu <106505838+Dwipandu@users.noreply.github.com>
2025-06-14 15:09:40 +0700

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -1,30 +1,30 @@
 const products = [
   {
     id: 1,
-    title: 'Abi Cabul',
-    price: 20000,
-    image: 'https://i.supaimg.com/031524d0-a2de-4662-9228-b49eb38d1639.jpg',
-    description: 'Buku Abi Cabul adalah kisah menarik tentang petualangan yang penuh kejutan.'
+    title: 'The psychology of money',
+    price: 55000,
+    image: 'asset/img1.jpg',
+    description: '"The Psychology of Money" karya Morgan Housel adalah buku self-development yang membahas bagaimana psikologi mempengaruhi keputusan keuangan kita. Buku ini tidak sekadar membahas pengelolaan keuangan, melainkan juga membahas bagaimana emosi, sikap, dan pengalaman masa lalu kita mempengaruhi cara kita berinteraksi dengan uang dan berinvestasi. '
   },
   {
     id: 2,
-    title: 'Kubil turu',
-    price: 85000,
-    image: 'https://i.supaimg.com/e5ab2de8-04c5-4b5d-9fdc-0492d3cf3410.jpg',
-    description: 'Kubil turu mengisahkan kehidupan sehari-hari dengan bahasa yang santai dan ringan.'
+    title: 'THE ART OF LIVING',
+    price: 45000,
+    image: 'asset/img2.jpg',
+    description: 'The Art of Living adalah panduan abadi yang menyaring ajaran Stoik dari filsuf Yunani kuno, Epictetus, menjadi nasihat praktis untuk kehidupan modern. Dengan gaya yang ringkas namun penuh makna, buku ini mengajak pembaca untuk menata hidup berdasarkan kebajikan, ketenangan batin, dan kebebasan sejati.'
   },
   {
     id: 3,
-    title: 'juliet romuna',
-    price: 950000,
-    image: 'https://i.supaimg.com/1b147f6d-afd8-4915-b62d-608936491de5.jpg',
+    title: 'is it bad or  good habits',
+    price: 40000,
+    image: 'asset/img3.jpg',
     description: 'Juliet Romuna adalah novel cinta penuh emosi dan konflik yang mendalam.'
   },
   {
     id: 4,
-    title: 'daus stang saher',
-    price: 120000,
-    image: 'https://i.supaimg.com/b7a6bf7b-984e-4ef1-8041-fd2432979238.jpg',
+    title: 'the principles of power',
+    price: 36000,
+    image: 'asset/img4.jpeg',
     description: 'Daus Stang Saher membahas teknologi terbaru dengan bahasa yang mudah dipahami.'
   }
 ];
